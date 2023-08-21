aee49b398da3c51741a41325af7dca9b92ae3a43
utkarsh-1235 <utkarsh.saxena13sept@gmail.com>
2023-08-21 15:11:57 +0530

    signup login page in react

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,12 +1,27 @@
 import './App.css'
+import { useState } from 'react';
 
 function App() {
  
+  const [login, setLogin] = useState(true);
+
+const loginSwitch = ()=>{
+    setLogin(!login);
+}
 
   return (
-    <>
-      
-    </>
+    <div id="login">
+      <div id="user">
+        <input hidden={login} placeholder="Enter the email id or phone" name="" id="" type="text" />
+        <input hidden={login} placeholder="Full Name" name="" id="" type="text" />
+        <input  placeholder="emailid ,phoneNo or userName " name="" id="" type="email" />
+        <input  placeholder="Password" name="" id="" type="text" />
+        <button>{login ? "signin" : "signup"}</button>
+        <div id="footer">
+          {login ? "Don't have an account" : "have an account"} <span onClick={loginSwitch}>{login ? "signup" : "login"}</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
